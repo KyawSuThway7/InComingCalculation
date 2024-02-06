c9a3ba961e030e7cbee5a5b822c96b6a419e755a
unknown <kyawsuthway.mtla2001@gmail.com>
2024-02-06 11:26:25 +0630

    first commit

---
+++ Register/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Register.Models;
+
+namespace Register.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+        [HttpPost]
+        public IActionResult Index(RegisterView register)
+        {
+            ViewBag.Restlt = $"Your register is successfully completed,Your Email is {register.Email}";
+            ViewBag.Name = register.Name;
+            ViewBag.Email = register.Email;
+            ViewBag.Password = register.Password;
+            ViewBag.ConfirmPassword = register.ConfirmPassword;
+            ViewBag.DateBirth = register.Date;
+            ViewBag.City = register.City;
+            ViewBag.Adress = register.Adress;
+      
+            return View();
+        }
+        
+    }
+}
---
+++ Register/Models/RegisterViews.cs
@@ -0,0 +1,14 @@
+﻿namespace Register.Models
+{
+    public class RegisterView
+    {
+        public string Name {  get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public bool Gender { get; set; }    
+        public DateOnly Date {  get; set; }
+        public string City {  get; set; }
+        public string Adress {  get; set; }
+    }
+}
