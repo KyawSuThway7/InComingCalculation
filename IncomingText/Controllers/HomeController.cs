c047ba046544d3485bf6bcae640f0529ec333874
unknown <kyawsuthway.mtla2001@gmail.com>
2024-02-09 01:15:44 +0630

    first commit

---
+++ IncomingText/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using IncomingText.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace IncomingText.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Result = null;
+            return View();
+        }
+        [HttpPost]
+        public JsonResult Index(totalAmount ta)
+        {
+            double result = 0;
+            if(ta.amount > 300000)
+            {
+                result = ta.amount / 100 * 10;
+                if(ta.father > 0 )
+                {
+                    result = result - 50000;
+                }
+                if(ta.mother > 0)
+                {
+                    result = result - 50000;
+                }
+                if(ta.Married > 0)
+                {
+                    result = result - 30000;
+                }
+                if(ta.children > 0)
+                {
+                    result = result - (ta.children * 30000);
+                }
+            }
+            return Json(result);
+           
+           
+        }
+    }
+}
---
+++ IncomingText/Models/totalAmount.cs
@@ -0,0 +1,11 @@
+﻿namespace IncomingText.Models
+{
+    public class totalAmount
+    {
+        public double amount { get; set; }
+        public double father {  get; set; }
+        public double mother {  get; set; }
+        public double Married { get; set; }
+        public double children { get; set; }
+    }
+}
